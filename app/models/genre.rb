6767ce78f7ebffd2a76a0c9a0addece9c32ddee5
Kristen Peck <kpeck71@gmail.com>
2017-12-01 03:20:24 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -12,6 +12,6 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    self.artists.name# return an array of strings containing every musician's name
   end
 end
